a6b771ac72e6bafdf7fd5796d631dcf157d23761
KevinWJohnson <kevin.johnson3@cox.net>
2018-06-27 17:40:32 -0700

    Next Arrival and Minutes Away calculated

--- assets/javascript/train_scheduler.js
+++ assets/javascript/train_scheduler.js
@@ -34,10 +34,14 @@ $("#submit-train").on("click", function (e) {
         frequency: trainFrequency
     });
 
+    // Clearing all the textboxes
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train-time").val("");
+    $("#frequency").val("");
+
 });
 
-// Need to calculte Next Arrival (nextArrival) and Minutes Away (minutesAway) - these have
-// all ready been put into the table below
 
 
 // Get all the records from the Firebase database and display that info
@@ -45,8 +49,38 @@ $("#submit-train").on("click", function (e) {
 
 database.ref().on("child_added", function (snapshot) {
 
+
+    // Need to calculte Next Arrival (nextArrival) and Minutes Away (minutesAway)
+    // using data from the Firebase database 
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(snapshot.val().firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % snapshot.val().frequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = snapshot.val().frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+
     console.log(snapshot.val());
 
-    $("table tbody").append("<tr><td>" + snapshot.val().name + "</td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().frequency + "</td><td>" + snapshot.val().nextArrival + "</td><td>" + snapshot.val().minutesAway +"</td></tr>");
+    $("table tbody").append("<tr><td>" + snapshot.val().name + "</td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().frequency + "</td><td>" + moment(nextTrain).format('hh:mm A') + "</td><td>" + tMinutesTillTrain + "</td></tr>");
 
 });
